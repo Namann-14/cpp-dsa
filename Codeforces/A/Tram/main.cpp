52d1d38b4975b28541da20703699953687ea0426
Naman <156062917+Namann-14@users.noreply.github.com>
2026-08-22 23:21:58 +0530

    Add main.cpp for Tram problem solution

---
+++ Codeforces/A/Tram/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int maxi = 0;
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+
+        sum -= a;
+        sum += b;
+
+        maxi = max(maxi, sum);
+    }
+
+    cout << maxi << endl;
+
+}
